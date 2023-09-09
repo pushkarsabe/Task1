73014c090775f7364c46d406722e201fa0091dc4
Pushkar Sabe <83827883+pushkarsabe@users.noreply.github.com>
2023-09-09 12:04:51 +0530

    Done with the main branch

--- Addition.java
+++ Addition.java
@@ -4,6 +4,7 @@ public class Main{
     int num1 = 5;
     int num2 = 2;
     int num3 = 13;
+    System.out.println("calculator");
     System.out.println("addiotion = "+(num1+num2+num3));
     System.out.println("subtraction = "+(num1-num2));
     System.out.println("multiplication = "+(num1*num2));
