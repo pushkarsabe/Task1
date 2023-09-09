4c37ff062064ad7943e58b06d2b371c6096e5453
Pushkar Sabe <83827883+pushkarsabe@users.noreply.github.com>
2023-09-09 11:32:08 +0530

    Create Addition.java
    Addition program in java

---
+++ Addition.java
@@ -0,0 +1,10 @@
+package com.soft;
+public class Main{
+  public static void main(String[] args){
+    int num1 = 5;
+    int num2 = 2;
+    int num3 = 13;
+    System.out.println("addiotion = "+(num1+num2+num3));
+    System.out.println("subtraction = "+(num1-num2-num3));
+  }//main
+}//class
