d918b3b28b0a73e2b49332f34d8e91855bb8ef04
Pushkar Sabe <83827883+pushkarsabe@users.noreply.github.com>
2023-09-09 11:40:28 +0530

    subtraction added

--- Addition.java
+++ Addition.java
@@ -5,6 +5,6 @@ public class Main{
     int num2 = 2;
     int num3 = 13;
     System.out.println("addiotion = "+(num1+num2+num3));
-    System.out.println("subtraction = "+(num1-num2-num3));
+    System.out.println("subtraction = "+(num1-num2));
   }//main
 }//class
